6267a5e4b2fbe9117615686465e5dfa5eb605716
mpres <michael.p.presley@gmail.com>
2026-01-14 14:58:21 -0800

    commit_message

--- Module_2/CSC-515_Module_2_CR_Option_1.py
+++ Module_2/CSC-515_Module_2_CR_Option_1.py
@@ -1,15 +1,17 @@
 import cv2 as cv
 
+#Step 1, extract color channels
 img = cv.imread("dog.jpg")
 
 b, g, r = cv.split(img)
 
-cv.imwrite('bluedog.png',b)
+#Step 2, remerge the color channels into one image 
 
 img_merged = cv.merge([b, g, r])
 
 cv.imwrite("merged.png",img_merged)
 
+#Step 3, swap the red and green channel
 img_r_g_swap = cv.merge([b, r, g])
 
 cv.imwrite('dog_r_g_swap.png',img_r_g_swap)
