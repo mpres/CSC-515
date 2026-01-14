6d5a87944eb3414f422bc7469332db8783e51f1d
mpres <michael.p.presley@gmail.com>
2026-01-14 14:55:50 -0800

    commit_message

--- Module_2/CSC-515_Module_2_CR_Option_1.py
+++ Module_2/CSC-515_Module_2_CR_Option_1.py
@@ -1,4 +1,15 @@
 import cv2 as cv
 
 img = cv.imread("dog.jpg")
-print(img)
+
+b, g, r = cv.split(img)
+
+cv.imwrite('bluedog.png',b)
+
+img_merged = cv.merge([b, g, r])
+
+cv.imwrite("merged.png",img_merged)
+
+img_r_g_swap = cv.merge([b, r, g])
+
+cv.imwrite('dog_r_g_swap.png',img_r_g_swap)
