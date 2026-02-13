91e3c34915525084467ecedeb09aaaee0ba15680
mpres <michael.p.presley@gmail.com>
2026-02-13 09:01:58 -0800

    create background image

--- Module_7/DQ_Module_7.py
+++ Module_7/DQ_Module_7.py
@@ -1,4 +1,9 @@
 import cv2 as cv
 import numpy as np
 
+# --- Settings ---
+IMG_WIDTH, IMG_HEIGHT = 500, 500
+BACKGROUND_COLOR = (255, 0, 0)   # Blue in BGR format (OpenCV uses BGR)
 
+
+image = np.full((IMG_HEIGHT, IMG_WIDTH, 3), BACKGROUND_COLOR, dtype=np.uint8)
