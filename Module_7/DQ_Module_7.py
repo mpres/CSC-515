fee2358961a8006ad8c5a891094272536cd4ebe8
mpres <michael.p.presley@gmail.com>
2026-02-13 09:03:57 -0800

    turn cv to cv2

--- Module_7/DQ_Module_7.py
+++ Module_7/DQ_Module_7.py
@@ -1,4 +1,4 @@
-import cv2 as cv
+import cv2
 import numpy as np
 
 # --- Settings ---
@@ -7,3 +7,10 @@ BACKGROUND_COLOR = (255, 0, 0)   # Blue in BGR format (OpenCV uses BGR)
 
 
 image = np.full((IMG_HEIGHT, IMG_WIDTH, 3), BACKGROUND_COLOR, dtype=np.uint8)
+
+
+
+# --- Display the image ---
+cv2.imshow("Filled Square", image)
+cv2.waitKey(0)        # Wait until a key is pressed
+cv2.destroyAllWindows()
