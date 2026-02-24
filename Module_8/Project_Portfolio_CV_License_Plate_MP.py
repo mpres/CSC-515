3953f7125079465c0666bdc6a08638f4f1b75462
mpres <michael.p.presley@gmail.com>
2026-02-23 16:30:05 -0800

    add model

--- Module_8/Project_Portfolio_CV_License_Plate_MP.py
+++ Module_8/Project_Portfolio_CV_License_Plate_MP.py
@@ -9,5 +9,5 @@ import pytesseract
 from pathlib import Path
 
 # ─── Paths ───────────────────────────────────────────────────────────────────
-CASCADE_PLATE_RUS  = "/usr/local/lib/python3.12/dist-packages/cv2/data/haarcascade_license_plate_rus_16stages.xml"
-CASCADE_PLATE_NUM  = "/usr/local/lib/python3.12/dist-packages/cv2/data/haarcascade_russian_plate_number.xml"
+CASCADE_PLATE_RUS  = "Models/haarcascade_russian_plate_number.xml"
+CASCADE_PLATE_NUM  = "Models/haarcascasde_license_plate_rus_16stages.xml"
