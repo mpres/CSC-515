9e2189f97085029e9b8447370e8ab10955ad1af6
mpres <michael.p.presley@gmail.com>
2026-02-24 13:46:05 -0800

    create detect_license_plate function

--- Module_8/Project_Portfolio_CV_License_Plate_MP.py
+++ Module_8/Project_Portfolio_CV_License_Plate_MP.py
@@ -8,6 +8,10 @@ import numpy as np
 import pytesseract
 from pathlib import Path
 
+SCALE_FACTOR = 1.25
+MIN_NEIGHBORS = 3
+
+
 # Paths for Models
 CASCADE_PLATE_RUS  = "Models/haarcascade_russian_plate_number.xml"
 CASCADE_PLATE_NUM  = "Models/haarcascasde_license_plate_rus_16stages.xml"
@@ -21,6 +25,7 @@ IMAGES = {
     "non_russian_multi":   "images/license_plates_non_russian.jpg",
     "russian_multi_far":   "images/Russian_Multi_far.png",
     "russian_upclose":     "images/Russian_License_Upclose.jpg",
+    "european_plate":     "images/European_plate.jpg",
 }
 
 
@@ -38,111 +43,56 @@ print("Cascade classifiers loaded")
 OUTPUT_DIR = Path("Outputs/")
 OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
+def detect_license_plate(img, model=cascade_rus, scale_factor=SCALE_FACTOR, min_Neighbors=MIN_NEIGHBORS):
+    ''' get image plates'''
+    detection_img = img.copy()
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Show gray image
+    cv2.imshow("Gray Image",img_gray)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    plates = model.detectMultiScale(img_gray, flags=cv2.CASCADE_SCALE_IMAGE, scaleFactor=scale_factor,
+                                          minNeighbors=min_Neighbors)
+    for (x, y, w, h) in plates:
+        cv2.rectangle(detection_img, (x, y), (x + w, y + h), (0, 0, 255), 3)
+
+    cv2.imshow("Result_Image", detection_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return detection_img, (x, y, w, h)
+
+
+
 
 #Get basic image
 
 img_path = Path(IMAGES["russian_upclose"])
 img_color = cv2.imread(img_path)
+
 if img_color is None:
         print(f"  ERROR: Could not read {img_path}")
+else:
+    detection_img = detect_license_plate(img_color)[0]
 
-img_gray = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
-
-
-#
-
-cv2.imshow("Image", img_gray)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-#plates = detect_plates(gray_proc, cascade_rus, scaleFactor=1.05, minNeighbors=3, minSize=(60, 20))
-plates = cascade_rus.detectMultiScale(img_gray,flags=cv2.CASCADE_SCALE_IMAGE, scaleFactor=1.4, minNeighbors=5)
-
-#cv2.imshow("Image_Plates", plates)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
-#print(plates)
-
-
-def detect_license_plate(img, plates):
-    detection_img = img.copy()
-    for (x, y) in plates:
-        cv2.rectangle(detection_img, (x, y), (x + w, y + h), (0, 0, 255), 3)
-    return detection_img
-
-
-
-
-detection_img = img_color.copy()
-for (x, y, w, h) in plates:
-    cv2.rectangle(detection_img, (x, y), (x + w, y + h), (0, 0, 255), 3)
-
-
-cv2.imshow("Image", detection_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-#im.show('test"',detection_img)
-
-
-
-#detect raw away plates
+#Get Russian car plates faraway
 
 img_path = Path(IMAGES["russian_multi_far"])
 img_color = cv2.imread(img_path)
-img_gray = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow("Image", img_gray)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-#plates = detect_plates(gray_proc, cascade_rus, scaleFactor=1.05, minNeighbors=3, minSize=(60, 20))
-plates = cascade_rus.detectMultiScale(img_gray,flags=cv2.CASCADE_SCALE_IMAGE, scaleFactor=1.4, minNeighbors=5)
-
-detection_img = img_color.copy()
-for (x, y, w, h) in plates:
-    cv2.rectangle(detection_img, (x, y), (x + w, y + h), (0, 0, 255), 3)
-
-cv2.imshow("Image", detection_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if img_color is None:
+        print(f"  ERROR: Could not read {img_path}")
+else:
+    detection_img = detect_license_plate(img_color)[0]
 
 
+#european_plate
 
-
-# Detect non-russian plats
-img_path = Path(IMAGES["non_russian_multi"])
+img_path = Path(IMAGES["european_plate"])
 img_color = cv2.imread(img_path)
-img_gray = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow("Image", img_gray)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-#plates = detect_plates(gray_proc, cascade_rus, scaleFactor=1.05, minNeighbors=3, minSize=(60, 20))
-plates = cascade_rus.detectMultiScale(img_gray,flags=cv2.CASCADE_SCALE_IMAGE, scaleFactor=1.4, minNeighbors=3)
-
-#if len(plates) > 0:
-#    plates = cascade_r.detectMultiScale(img_gray, flags=cv2.CASCADE_SCALE_IMAGE, scaleFactor=1.4, minNeighbors=5)
-
-
-
-detection_img = img_color.copy()
-for (x, y, w, h) in plates:
-    cv2.rectangle(detection_img, (x, y), (x + w, y + h), (0, 0, 255), 3)
-
-cv2.imshow("Image", detection_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
-plates = cascade_num.detectMultiScale(img_gray, scaleFactor=1.01, minNeighbors=4)
-
-
-detection_img = img_color.copy()
-
-for (x, y, w, h) in plates:
-    cv2.rectangle(detection_img, (x, y), (x + w, y + h), (0, 0, 255), 3)
-
-cv2.imshow("Image", detection_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+if img_color is None:
+        print(f"  ERROR: Could not read {img_path}")
+else:
+    detection_img = detect_license_plate(img_color)[0]
