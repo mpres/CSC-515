1186246c2fd7773ffde06c4d3aacb05435c211af
mpres <michael.p.presley@gmail.com>
2026-02-23 16:59:40 -0800

    add image paths

--- Module_8/Project_Portfolio_CV_License_Plate_MP.py
+++ Module_8/Project_Portfolio_CV_License_Plate_MP.py
@@ -8,6 +8,28 @@ import numpy as np
 import pytesseract
 from pathlib import Path
 
-# ─── Paths ───────────────────────────────────────────────────────────────────
+# Paths for Models
 CASCADE_PLATE_RUS  = "Models/haarcascade_russian_plate_number.xml"
 CASCADE_PLATE_NUM  = "Models/haarcascasde_license_plate_rus_16stages.xml"
+
+#  Load Cascades Models
+cascade_rus  = cv2.CascadeClassifier(CASCADE_PLATE_RUS)
+cascade_num  = cv2.CascadeClassifier(CASCADE_PLATE_NUM)
+
+# get Images
+IMAGES = {
+    "non_russian_multi":   "images/license_plates_non_russian.jpg",
+    "russian_multi_far":   "/images/Russian_Multi_far.png",
+    "russian_upclose":     "/images/Russian_Upclose.png",
+}
+
+#check models
+if cascade_rus.empty() or cascade_num.empty():
+    raise RuntimeError("Failed to load cascade classifiers!")
+
+print("Cascade classifiers loaded")
+
+
+# ─── Helper: CLAHE Preprocessing ─────────────────────────────────────────────
+
+print(IMAGES['non_russian_multi'])
